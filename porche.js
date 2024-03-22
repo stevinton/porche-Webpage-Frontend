4c75dc2012f7db9d14c3252386ba5b8711da186d
stevinton <stevinton9@gmail.com>
2024-03-22 20:27:51 +0530

    after device change

--- porche.js
+++ porche.js
@@ -7,6 +7,7 @@ const arrow = document.querySelectorAll(".arrow");
 let transitionVal = 0;
 
 const right = () => {
+<<<<<<< HEAD
     if (transitionVal === -5472) {
         transitionVal = 0;
     } else {
@@ -14,11 +15,21 @@ const right = () => {
     }
     img.forEach(e => {
         e.style.transform = `translateX(${transitionVal}px)`;
+=======
+    if (transitionVal === -400) {
+        transitionVal = 0;
+    } else {
+        transitionVal += -100;
+    }
+    img.forEach(e => {
+        e.style.transform = `translateX(${transitionVal}vw)`;
+>>>>>>> 26a446b (sec commit)
         e.style.transition = 'transform 1.5s ease-in-out';
     });
 }
 
 const autoTransition = () => {
+<<<<<<< HEAD
     if (transitionVal === -5472) {
         transitionVal = 0;
     } else {
@@ -26,6 +37,15 @@ const autoTransition = () => {
     }
     img.forEach(e => {
         e.style.transform = `translateX(${transitionVal}px)`;
+=======
+    if (transitionVal === -400) {
+        transitionVal = 0;
+    } else {
+        transitionVal += -100;
+    }
+    img.forEach(e => {
+        e.style.transform = `translateX(${transitionVal}vw)`;
+>>>>>>> 26a446b (sec commit)
         e.style.transition = 'transform 1.5s ease-in-out';
     });
     dead();
@@ -37,12 +57,21 @@ const dead = () => {
 
 const left = () => {
     if (transitionVal === 0) {
+<<<<<<< HEAD
         transitionVal += -5472
     } else {
         transitionVal += 1368
     }
     img.forEach(e => {
         e.style.transform = `translateX(${transitionVal}px)`;
+=======
+        transitionVal += -400
+    } else {
+        transitionVal += 100
+    }
+    img.forEach(e => {
+        e.style.transform = `translateX(${transitionVal}vw)`;
+>>>>>>> 26a446b (sec commit)
         e.style.transition = 'transform 1.5s ease-in-out';
     });
 }
@@ -65,9 +94,19 @@ img.forEach(imgs => {
     });
 });
 
+<<<<<<< HEAD
 // const modelBuildBtn = document.querySelector(".btn");
 
 // modelBuildBtn.addEventListener('click', function () {
 //     window.location.href = "https://www.porsche.com/middle-east/_india_/modelstart/all/?modelrange=718";
 //     return true;
 // });
+=======
+const moedelImage = document.querySelectorAll(".btn");
+
+moedelImage.forEach(imgm =>{
+    imgm.addEventListener('mouseover', function(){
+        
+    })
+})
+>>>>>>> 26a446b (sec commit)
